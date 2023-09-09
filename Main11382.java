5372436da94a2b5a29edf8b4e03c47fc9bc38e9f
jk <yjkeong@gmail.com>
2023-09-09 21:13:38 +0900

    first commit

---
+++ Main11382.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main11382{
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    long a, b, c;
+    a = sc.nextLong();
+    b = sc.nextLong();
+    c = sc.nextLong();
+   
+    sc.close();
+
+    System.out.println(a+b+c);
+  }
+}
